7ad88422dd22544f32dbcd44eac9953acf9e5733
satyamkodale <satyamkodale@gmail.com>
2024-06-06 16:45:27 +0530

    updated all files

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav/Nav.jsx";
-
 import "./App.css";
 // ------------------------------------------------------
 import Skills from "./components/Skills/Skills.jsx";
